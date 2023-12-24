1a10342a0f45a0087772f7b6cd8c60c2b9c3a277
THISISJUSTICE <yulop64@naver.com>
2023-12-24 16:41:00 +0900

    MapTool
    맵 생성 에디터 개발

--- Assets/TestCollision.cs
+++ Assets/TestCollision.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Playables;
 using UnityEngine;
+using UnityEngine.Diagnostics;
 using UnityEngine.Tilemaps;
 
 public class TestCollision : MonoBehaviour

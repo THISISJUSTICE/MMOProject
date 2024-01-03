18c0add05d7e1ab04243e759f48b905ecb199846
THISISJUSTICE <yulop64@naver.com>
2024-01-03 18:05:16 +0900

    멀티 플레이 환경
    ServerCore.dll 파일을 지우고 ServerCore에서 필요한 스크립트를 적절히 추가
    - dll 파일도 해커가 들여다볼 수 있기에 로컬 영역에 전달하는 스크립트를 최소화
    
    유니티 에디터에 한 번에 여러 개를 빌드할 수 있도록 자동화 스크립트 추가

--- Assets/Scripts/Packet/ServerSession.cs
+++ Assets/Scripts/Packet/ServerSession.cs
@@ -12,20 +12,6 @@ public class ServerSession : PacketSession
 	public override void OnConnected(EndPoint endPoint)
 	{
 		Debug.Log($"OnConnected : {endPoint}");
-
-		S_Chat chat = new S_Chat()
-		{
-			Context = "안녕하세요"
-		};
-
-		ushort size = (ushort)chat.CalculateSize();
-		byte[] sendBuffer = new byte[size + 4];
-		Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
-		ushort protocolID = (ushort)MsgId.SChat;
-		Array.Copy(BitConverter.GetBytes(protocolID), 0, sendBuffer, 2, sizeof(ushort));
-		Array.Copy(chat.ToByteArray(), 0, sendBuffer, 4, size);
-		Debug.Log($"chat size: {chat.CalculateSize()}");
-		Send(new ArraySegment<byte>(sendBuffer));
 	}
 
 	public override void OnDisconnected(EndPoint endPoint)
